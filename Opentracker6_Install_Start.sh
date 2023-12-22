b3d2f578ad1bdcba2a984c22d116c6dbd7b0a09b
仁二 <17070453@qq.com>
2023-12-23 07:27:17 +0800

    更新 Opentracker6_Install_Start.sh
    修复脚本执行 -eq / -ne 状态判断反了
    测试不支持睡觉 一并取消
    判断内容 返回：”0“ 更容易识别

--- Opentracker6_Install_Start.sh
+++ Opentracker6_Install_Start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #查找opentracker6安装路径;如果返回0,则开始安装更新;安装后写入txt判断内容:是否安装成功。
 which opentracker6
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 	logger -t "开始更新opentracker6"
 	opkg update && opkg install opentracker6 | tee opt6_log.txt
 	if [ -n "$(grep "Configuring opentracker" opt6_log.txt)" ]; then
@@ -14,14 +14,14 @@ if [ $? -eq 0 ]; then
 fi
 
 #我睡5秒觉 没毛病吧？
-logger -t "正在启动...opentracker6"
-sleep 5
+#logger -t "正在启动...opentracker6"
+#sleep 5
 
 #查找opentracker6安装路径;如果返回不等于0,则启动;检测进程连接是否为0,不等于0则启动成功,反之失败。
 which opentracker6
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
 	result=$(netstat -apn|grep opentracker | wc -l)
-	if [ $result -ne 0 ]; then
+	if [ $result = "0" ]; then
 		#ipv6监听tcp:233、tcp:2710+6969
 		opentracker6 -p 233 -P 233 -p 2710 -p 6969 &
 		#ipv6监听tcp:666、udp:2710+6969
